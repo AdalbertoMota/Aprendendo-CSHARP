5d25a5fc89b419916e46cf203cdf6286fae6cdf9
Adalberto Mota <adalberto.mota@uni9.edu.br>
2021-10-05 23:10:55 -0300

    Estudando

---
+++ 5-CaracteresETexto/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _5_CaracteresETexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 5 - Caracteres e Textos!");
+            Console.ReadLine();
+
+            //character
+            char primeiraLetra = 'a';
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)65;
+            Console.WriteLine(primeiraLetra);
+
+            primeiraLetra = (char)(primeiraLetra + 1);
+            Console.WriteLine(primeiraLetra);
+
+            string titulo = "Alura Cursos de Tecnologia " + 2021;
+            string cursosProgramacao = 
+@"
+- .NET
+- Java  
+- JavaScript 
+- HTML e CSS";
+
+            Console.WriteLine(titulo);
+            Console.WriteLine(cursosProgramacao);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
